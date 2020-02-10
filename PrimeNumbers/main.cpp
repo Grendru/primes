db4416c1c1ba962cc080092a246b11ad8bd52941
Евгений Мещеряков <eugene.meshch@yandex.ru>
2020-02-11 02:36:38 +0300

    PrintToFIle

--- PrimeNumbers/main.cpp
+++ PrimeNumbers/main.cpp
@@ -1,28 +1,53 @@
 ﻿#include <iostream>
+#include <fstream>
 #include "primes.h"
 
 using namespace std;
 
+void PrintToFile(char* filename, class Primes* myPrimes)
+{
+	ofstream file;
+	file.open(filename, ofstream::out);
+	for (auto it : *myPrimes)
+	{
+		file << it << " " << endl;
+	}
+	file.close();
+}
 int main(int argc, char* argv[])
 {
-	cout << argc << endl << argv[1] << endl;
+	//cout << argc << endl << argv[1] << endl;
+	class Primes* myPrimes = NULL;
 	if (argc == 1) //запуск приложения по умолчанию
 	{
-		class Primes myPrimes(100,'r');
-		//class Primes::Iterator it(myPrimes.begin());
-		for (auto it : myPrimes)
+		class Primes temp(100,'r');
+		for (auto it : temp)
 		{
 			cout << it << " " << endl;
 		}
 	}
 	else
 	{
-		if (argv[1][0] == '-')
+		bool print = false;
+		for (uint32_t i = 0; i < argc; i++)
 		{
-			class Primes myPrimes(atoi(argv[2]), argv[1][1]);
-			for (auto it : myPrimes)
+			if (argv[i][0] == '-' && (argv[i][1] == 'r' || argv[i][1] == 'q'))
 			{
-				cout << it << " " << endl;
+				class Primes temp(atoi(argv[i+1]), argv[i][1]);
+				myPrimes = &temp;
+				print = true;
+			}
+			else if (argv[i][0] == '-' && argv[i][1] == 'o')
+			{
+				if (print) PrintToFile(argv[i + 1], myPrimes);
+				print = false;
+			}
+		}
+		if (print)
+		{
+			for (auto it : *myPrimes)
+			{
+				cout << it << " ";
 			}
 		}
 	}
