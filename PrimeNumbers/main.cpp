d404825f90d8f54f4b8f752b3293cfa3c7c63af0
Евгений Мещеряков <eugene.meshch@yandex.ru>
2020-02-11 04:11:53 +0300

    LambdaFunction

--- PrimeNumbers/main.cpp
+++ PrimeNumbers/main.cpp
@@ -26,9 +26,10 @@ void PrintToFile(char* filename, class Primes myPrimes, char mode)
 	}
 	else if (mode == 'g')
 	{
+		auto SophieLambda = [](uint32_t p) { return IsPrime(2 * p + 1); };
 		for (auto it : myPrimes)
 		{
-			if ( IsPrime(2*it + 1) )file << it << endl;
+			if (SophieLambda(it) )file << it << endl;
 		}
 	}
 	file.close();
@@ -40,21 +41,28 @@ int main(int argc, char* argv[])
 	char modePrint = 'n';
 	char modeGenerate = 'r';
 	uint32_t size = 100;
-	for (uint32_t i = 0; i < argc; i++)
+	for (uint32_t i = 1; i < argc; i++)
 	{
-		if (argv[i][0] == '-' && (argv[i][1] == 'r' || argv[i][1] == 'q'))
+		if ((argv[i][0] == '-' && (argv[i][1] == 'r' || argv[i][1] == 'q')) && argv[i][2] == '\0' && (argv[i+1][0] >= '0' && argv[i + 1][0] <= '9'))
 		{
 			size = atoi(argv[i + 1]);
 			modeGenerate = argv[i][1];
+			i++;
 		}
-		else if (argv[i][0] == '-' && argv[i][1] == 's' && (argv[i][2] == 'g' || argv[i][2] == 'p'))
+		else if (argv[i][0] == '-' && argv[i][1] == 's' && (argv[i][2] == 'g' || argv[i][2] == 'p') && argv[i][3] == '\0')
 		{
 			modePrint = argv[i][2];
 		}
-		else if (argv[i][0] == '-' && argv[i][1] == 'o')
+		else if (argv[i][0] == '-' && argv[i][1] == 'o' && argv[i][2] == '\0' && (argv[i + 1][0] >= 'a' && argv[i + 1][0] <= 'z' || argv[i + 1][0] >= 'A' && argv[i + 1][0] <= 'Z' || argv[i + 1][0] >= '0' && argv[i + 1][0] <= '9'))
 		{
 			FileName = argv[i + 1];
 			print = true;
+			i++;
+		}
+		else
+		{
+			cout << "[error]: Unknown command \"" << argv[i] << "\"" << endl;
+			exit(1);
 		}
 	}
 	class Primes myPrimes(size, modeGenerate);
@@ -64,9 +72,26 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		for (auto it : myPrimes)
+		if (modePrint == 'n')
 		{
-			cout << it << " ";
+			for (auto it : myPrimes)
+			{
+				cout << it << " ";
+			}
+		}
+		else if (modePrint == 'p')
+		{
+			for (uint32_t i = 0; myPrimes[i] <= myPrimes.size(); i++)
+			{
+				cout << myPrimes[myPrimes[i]] << " ";
+			}
+		}
+		else if (modePrint == 'g')
+		{
+			for (auto it : myPrimes)
+			{
+				if (IsPrime(2 * it + 1)) cout << it << " ";
+			}
 		}
 	}
 }
